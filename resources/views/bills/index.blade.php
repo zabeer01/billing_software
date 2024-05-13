d260db9fe402f2ff9da5a57c9be64e5ff847b2d4
Syed Abdullah Zabir <ibnejabir001@gmail.com>
2024-05-13 20:12:14 +0600

    updated

--- resources/views/bills/index.blade.php
+++ resources/views/bills/index.blade.php
@@ -1,11 +1,58 @@
-<?php
-foreach ($customers as $customer) {
-    echo $customer->name . "<br>";
-}
-foreach ($customerIds as $customerId) {
-    echo $customerId . "<br>";
-}
-foreach ($websites as $website) {
-    echo $website->name . "<br>";
-}
-?>
+@extends('layouts.app2')
+
+@section('content')
+    <div class="container">
+        <table class="table table-bordered" id="websiteTable">
+
+            <thead>
+                <tr>
+                    <th>
+                        <input class="form-check-input" type="checkbox" id="selectAllCheckbox">
+                    </th>
+                    <th>no</th>
+                    <th>Website Name</th>
+                    <th>Website URL</th>
+                    <th>Bill</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($websites as $website)
+                    <tr>
+                        <td>
+                            <input class="form-check-input websiteCheckbox" type="checkbox" value="{{ $website->id }}">
+                        </td>
+                        <td>{{ $website->id }}</td>
+                        <td>{{ $website->name }}</td>
+                        <td>{{ $website->url }}</td>
+                        <td class="bill">{{ $website->bill }}</td>
+                    </tr>
+                    
+                @endforeach
+                <tr>
+                    <td></td><td></td><td></td>
+                   
+                    <td>total </td>
+                    <td id='total'></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            console.log('hi');
+        });
+    </script>
+@endsection
+@section('scripts')
+    <script>
+        $(document).ready(function() {
+            // Calculate total bill on DOM ready
+            var total = 0;
+            $('#websiteTable tbody .bill').each(function() {
+                total += parseFloat($(this).text());
+            });
+            $('#total').text(total);
+        });
+    </script>
+@endsection
+
