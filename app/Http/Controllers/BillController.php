8bcd30f578a1ab93cf977258a7208b8feae0813b
Syed Abdullah Zabir <ibnejabir001@gmail.com>
2024-06-10 13:04:28 +0600

    e

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -90,10 +90,7 @@ class BillController extends Controller
         $ajaxData = session()->get('ajaxData');
         session()->flush();  
         $billingWebsiteID = $ajaxData['billingWebsiteID'];  
-        /* $billedWebsites = Website::all(); */
-        //filtered websites
         $billedWebsites  = Website::whereIn('id',$billingWebsiteID)->get();
-        
         return view('bills.invoice', compact('billedWebsites'));
     }
     
